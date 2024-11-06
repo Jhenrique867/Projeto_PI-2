d87e2cdf23894382d9db5cb78921a2fcfd131074
Levy <levisilvas2018@gmail.com>
2024-11-06 16:30:50 -0300

    formulario de cadastro de produtos

--- app_produtos/views.py
+++ app_produtos/views.py
@@ -1,22 +1,18 @@
 from django.shortcuts import render, redirect
 from .models import Produto
+from . import forms
 
 def cad_produtos(request):
-    return render (request, 'produtos/cadprodutos.html')
+    if request.method == 'POST':
+        form = forms.ProdutoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('produtos:lista_produtos')
+    else:
+        form = forms.ProdutoForm()
+    return render(request, 'produtos/cadprodutos.html', {'form': form})
+    
 
-
-# Não sei se é desse jeito mais vou deixar assim por enquanto
 def produto(request):
     produtos = Produto.objects.all()
     return render(request, 'produtos/produtos.html', {'produtos': produtos})
-
-# essa função tá fazendo dar erro ao selecionar o botão de produtos
-
-# def produto(request):
-#     novo_produto = Produto()
-#     novo_produto.descricao = request.POST.get('descricao')
-#     novo_produto.fornecedor = request.POST.get('ornecedor')
-#     novo_produto.custo = request.POST.get('custo')
-#     novo_produto.lucro = request.POST.get("lucro")
-#     novo_produto.valor_de_venda =request.POST.get("valor venda")
-#     novo_produto.marca = request.POST.get("marca")
